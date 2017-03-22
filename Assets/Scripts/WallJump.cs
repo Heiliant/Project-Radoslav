713649796d91e9489887b3188aa91970a8708069
Heiliant <alexbalasteguigaya@enti.cat>
2017-03-23 00:29:36 +0100

    Walljump "acabado"
    He dejado el walljump mas menos apañado. He tenido que tocar el
    funcionamiento de los controles básicos de movimiento.

--- Assets/Scripts/WallJump.cs
+++ Assets/Scripts/WallJump.cs
@@ -3,58 +3,78 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class WallJump : MonoBehaviour {
-    public GameObject prota;            //Aqui meto el gameobject del PJ para poder retocar sus propiedades desde este script
-    public ControladorProtagonista script; //Esto del script sirve para acceder al script "ControladorProtagonista" y asi poder hacer el booleno
-    private float fuerzaY=1;
-    public float incrementoCaida = 1;// que contiene y que regula el sistema de salto sea true cuando el PJ toca una pared
-    private float contador=0;
-    public float segToFall = 1;
-    private bool controlcontador = true;
+public class WallJump : MonoBehaviour
+{
+    public GameObject prota;
+    public PlayerControl script;
 
-	// Use this for initialization
-	void Start () {
-        script = prota.GetComponent<ControladorProtagonista>();
-	}
+    private float FuerzaYOriginal = 1;
+    public float fuerzaY;//
+    public float incrementoCaida = 1;
+    public float contador = 0;//
+    public float segToFall = 1;
+
+    public bool derecha;
+    public int sentido;//
+
+    // Use this for initialization
+    void Start()
+    {
+        script = prota.GetComponent<PlayerControl>();
+
+        fuerzaY = FuerzaYOriginal;
+
+        if (derecha)
+            sentido = 1;
+        else
+            sentido = -1;
+    }
 
     private void FixedUpdate()
     {
-        if (controlcontador)
+
+        if (!script.getEnpared())
         {
             contador = 0;
-            controlcontador = false;
+            fuerzaY = FuerzaYOriginal;
         }
     }
 
-    // Update is called once per frame
-    void Update () {
-		
-	}
 
-    private void OnCollisionStay2D(Collision2D collision) //OnCollisionStay2D se llama cuando el collider de este gameObject entra en contacto con otro
-                           //collider. El gameobject de este collider se trata a través de "collision"
+    void Update()
     {
-        
+
+
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+
+    {
+
         if (collision.gameObject.tag == "Player")
         {
+
             contador += Time.deltaTime;
-            if (contador >= segToFall)
+            if (contador > segToFall)
             {
-                fuerzaY -= Time.deltaTime * incrementoCaida;
+                fuerzaY -= (Time.deltaTime * incrementoCaida);
             }
-            
-            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A )) //hay que retocar el script para que al mover al PJ en direccion opuesta a la pared
-                                                        //este no se mantenga en el aire por un momento antes de caer.
+
+
+            if ((Input.GetKey(KeyCode.D) && (!derecha)) || ((derecha) && Input.GetKey(KeyCode.A)))
+
             {
-                prota.GetComponent<Rigidbody2D>().velocity = new Vector2(collision.gameObject.GetComponent<Rigidbody2D>().velocity.x, fuerzaY);  //Esto es para que el PJ no se caiga al tocar la pared. 
-                                                                                //Aun asi resbala un poquito, pero creo que queda bien. Se puede retocar.
-                script.setenSuelo(true);
+                prota.GetComponent<Rigidbody2D>().velocity = new Vector2(0/*collision.gameObject.GetComponent<Rigidbody2D>().velocity.x*/, fuerzaY);
+
+
+                if (Input.GetKey(KeyCode.Space) && script.getEnpared())
+                {
+
+                    prota.GetComponent<Rigidbody2D>().velocity = new Vector2(script.getFuerzawalljump()*sentido*2, script.getFuerzawalljump()*1.5f);
+                }
+
             }
         }
-    }
 
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        controlcontador = true;
     }
 }
