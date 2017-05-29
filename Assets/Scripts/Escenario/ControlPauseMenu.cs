261aae5ea7af46974dc1d919b1d035d06452fe96
Heiliant <alexbalasteguigaya@enti.cat>
2017-05-30 00:24:48 +0200

    Menú de pausa a media. Faltan las opciones, que acabarán por ser un submenú.

---
+++ Assets/Scripts/Escenario/ControlPauseMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControlPauseMenu : MonoBehaviour {
+
+    private void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void _Continue()
+    {
+        gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void _Quit()
+    {
+        Application.Quit();
+    }
+    public void _Options()
+    {
+
+    }
+
+    public void _Reload()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1;
+    }
+}
