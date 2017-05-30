a0fd270021444d3b031f4f64b72ce2bebb7566e9
Heiliant <alexbalasteguigaya@enti.cat>
2017-05-30 11:57:51 +0200

    dunno

---
+++ Assets/Scripts/Menus/ControlChangeControls.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlChangeControls : MonoBehaviour {
+
+	public void _Back()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void _ChangeKey()
+    {
+        StartCoroutine(changeInput());
+    }
+
+    IEnumerator changeInput()
+    {
+        yield return new WaitUntil(Input.)
+    }
+}
