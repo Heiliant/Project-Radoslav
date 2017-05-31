6c11a7a20c6ad865a033d92a01753e8c3353e220
Heiliant <alexbalasteguigaya@enti.cat>
2017-05-31 13:41:03 +0200

    zwerxdvgybhunjmkl,ñwefgrthrnymhgj

--- Assets/Scripts/Enemigos/Dragon/ComportamientoDragon.cs
+++ Assets/Scripts/Enemigos/Dragon/ComportamientoDragon.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ComportamientoDragon : MonoBehaviour {
     public GameObject[] brazos;
     public GameObject[] antebrazos;
+    public GameObject[] manos;
     public GameObject player;
     // Use this for initialization
     void Start() {
@@ -13,26 +14,58 @@ public class ComportamientoDragon : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        foreach (GameObject a in brazos)
+        for (int i = 0; i < antebrazos.Length; ++i)
         {
             //vector unitario del hombro al PJ
-            Vector2 localVector = a.GetComponent<Transform>().position - player.GetComponent<Transform>().position;
-            localVector /= (Vector2.Distance(a.GetComponent<Transform>().position, player.GetComponent<Transform>().position));
+            Vector2 localVector = antebrazos[i].GetComponent<Transform>().position - player.GetComponent<Transform>().position;
+            localVector /= (Vector2.Distance(antebrazos[i].GetComponent<Transform>().position, player.GetComponent<Transform>().position));
 
-            //angulo entre localVec y Vector2.right
-            float angle=Mathf.Acos(((Vector2.right.x * localVector.x) + (Vector2.right.y * localVector.y)) / 
-                (Mathf.Sqrt(Vector2.right.x * Vector2.right.x + Vector2.right.y * Vector2.right.y) * 
-                Mathf.Sqrt(localVector.x * localVector.x + localVector.y * localVector.y)));
+
+            float angle = Mathf.Atan2(localVector.y, localVector.x);
             angle *= Mathf.Rad2Deg;
-            Physics2D.Linecast(a.GetComponent<Transform>().position, player.GetComponent<Transform>().position);
-            Debug.DrawLine(a.GetComponent<Transform>().position, player.GetComponent<Transform>().position, Color.red);
-            a.GetComponent<Transform>().Rotate(0, 0, angle);
+            angle -= 90;
 
+           
+
+                antebrazos[i].GetComponent<Transform>().rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+   
         }
-        foreach (GameObject a in antebrazos)
+
+        for (int i = 0; i < brazos.Length; ++i)
         {
-            Physics2D.Linecast(a.GetComponent<Transform>().position, player.GetComponent<Transform>().position);
-            Debug.DrawLine(a.GetComponent<Transform>().position, player.GetComponent<Transform>().position, Color.cyan);
+            //vector unitario del hombro al PJ
+            Vector2 localVector = brazos[i].GetComponent<Transform>().position - player.GetComponent<Transform>().position;
+            localVector /= (Vector2.Distance(brazos[i].GetComponent<Transform>().position, player.GetComponent<Transform>().position));
+
+
+            float angle = Mathf.Atan2(localVector.y, localVector.x);
+            angle *= Mathf.Rad2Deg;
+            angle -= 90;
+            
+            while (angle < 0)
+                angle += 360;
+            while (angle >= 360)
+                angle -= 360;
+
+            if (brazos[i].name.Equals("BrazoD4"))
+                Debug.Log(angle);
+            if (i % 2 == 0)
+            {
+                if ((angle >= 60 && angle <= 120))
+                    brazos[i].GetComponent<Transform>().rotation = Quaternion.AngleAxis(Mathf.Clamp(angle, 60, 120), Vector3.forward);
+                else if ((angle <= 300 && angle >= 240))
+                    brazos[i].GetComponent<Transform>().rotation = Quaternion.AngleAxis(Mathf.Clamp(360 - angle, 60, 120), Vector3.forward);
+            }
+            
+            else
+            {
+                if ((angle >= 60 && angle <= 120))
+                    brazos[i].GetComponent<Transform>().rotation = Quaternion.AngleAxis(Mathf.Clamp(360-angle, 240, 300), Vector3.forward);
+                else if ((angle <= 300 && angle >= 240))
+                    brazos[i].GetComponent<Transform>().rotation = Quaternion.AngleAxis(Mathf.Clamp(angle, 240, 300), Vector3.forward);
+            }
+
         }
     }
+    
 }
